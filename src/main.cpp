2d34a8f07d1692d10b7d8398e323f5fb0588dd3c
drmugg123 <drmugg123@protonmail.ch>
2016-01-05 03:38:56 +0100

    added jsoncpp for parsing the json data

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
+#include <fstream>
+
+//JsonCpp
+#include "json/json.h"
+
+bool loadJsonFile(const std::string& where, std::string& outString) {
+	std::ifstream inFile(where, std::ios::in);
+	std::string line, src;
+
+	if(inFile.is_open()) {
+		while(std::getline(inFile, line) && inFile.good()) {
+			src += line;// += "\n";
+		}
+
+		inFile.close();
+	} else {
+		std::cout << "Failed to open json file " << where << "\n";
+		return false;
+	}
+
+	outString = src;
+
+	return true;
+}
+
+class DataPresenter {
+	protected:
+		std::string data;
+	public:
+		DataPresenter(std::string toParse) : data(toParse) {
+		}
+		~DataPresenter() {
+		}
+
+		std::string getNicelyFormattedData() {
+			std::string result = data;
+			return result;
+		}
+};
 
 int main(void) {
-	std::cout << "hello world\n";
+	std::string data;
+
+	if(!loadJsonFile("data/ME0104T4", data)) {
+		return -1;
+	}
+
+	DataPresenter presenter(data);
+
+	std::cout << presenter.getNicelyFormattedData() << "\n";
 
 	return 0;
 }
